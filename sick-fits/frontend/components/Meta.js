a308bd5f271a1c4fcca112c58a2530746a669454
Jan Czizikow <jan.czizikow@gmail.com>
2018-10-22 19:17:23 +0200

    Fix: typo in Meta tags

--- sick-fits/frontend/components/Meta.js
+++ sick-fits/frontend/components/Meta.js
@@ -4,7 +4,7 @@ const Meta = () => (
   <Head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <link rel="shortcut icon" href="/static/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>Sick Fits!</title>
